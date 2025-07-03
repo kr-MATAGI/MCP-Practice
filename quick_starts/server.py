b696137c2ed687698f99ead5dbe642da9cd54a41
kr-MATAGI <dev_matagi@kakao.com>
2025-07-03 12:30:55 +0900

    [Init] server.py 추가

---
+++ quick_starts/server.py
@@ -0,0 +1,7 @@
+"""
+현재 참조 서버
+    - Filesystem : 구성 가능한 액세스 제어를 통한 안전한 파일 작업
+    - Fetch : LLM 사용에 최적화된 웹 콘텐츠 fetch 및 변환
+    - Memory : 지식 그래프 기반 지속 메모리 시스템
+    - Sequential Thinking : 사고 순서를 통한 역동적인 문제 해결
+"""
